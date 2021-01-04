0d8f56be885879e9d56a4206ce33170eeceb21d7
vomichaelminh <vomm@uci.edu>
2021-01-04 04:12:32 -0800

    adding files

--- server/index.js
+++ server/index.js
@@ -2,9 +2,11 @@ import express from "express";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
+import dotenv from "dotenv";
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+dotenv.config();
 
 app.use(bodyParser.json());
 app.use(cors());
@@ -12,6 +14,19 @@ app.use(cors());
 app.get("/", (req, res) => {
   res.send("Homepage");
 });
+
+mongoose.connect(
+  process.env.CONNECT_URL,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  },
+  () => {
+    console.log("Connected to db!");
+  }
+);
+
+mongoose.set("useFindAndModify", false);
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
