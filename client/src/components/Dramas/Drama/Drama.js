438e540fc59f0dafea293ce329e2e3f3c2878272
vomichaelminh <vomm@uci.edu>
2021-01-07 03:01:20 -0800

    fixed some css

--- client/src/components/Dramas/Drama/Drama.js
+++ client/src/components/Dramas/Drama/Drama.js
@@ -1,30 +1,14 @@
 import React from "react";
-import styled from "styled-components";
+import "./style.drama.css";
 
 const Drama = ({ title, image, description }) => {
   return (
-    <StyledDrama>
+    <div className="drama">
       <h4>{title}</h4>
       <img src={image} alt="drama_image" />
       <p>Description: {description}</p>
-    </StyledDrama>
+    </div>
   );
 };
-
-const StyledDrama = styled.div`
-  font-size: 1rem;
-  height: 30vh;
-  width: 30vh;
-  box-shadow: 0px 5px 20px rgba(0, 0, 0, 0.2);
-  text-align: center;
-  border-radius: 1rem;
-  cursor: pointer;
-  overflow: hidden;
-  img {
-    max-width: 100px;
-    max-height: 100px;
-    object-fit: cover contain;
-  }
-`;
 
 export default Drama;
